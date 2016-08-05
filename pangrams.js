2b3355d330436a061cf91ca8c46b785d60d5de02
Seth Bunke <seth.bunke@hotmail.com>
2016-08-05 17:57:54 +0000

    start pangram

--- pangrams.js
+++ pangrams.js
@@ -1,1 +1,31 @@
-ECHO is on.
+
+
+let array = 
+    [
+        {"name":"Joe", "age":17}, 
+        {"name":"Bob", "age":17}, 
+        {"name":"Carl", "age": 35}
+    ];
+
+const unique = [...new Set(array.map(item => item.age))];
+
+console.log(unique);
+
+
+let x = 'We promptly judged antique ivory buckles for the next prize';
+
+
+let str = x.toLowerCase().replace(/\s/g, '').split('');
+
+console.log(str);
+
+
+const uniqueChars = [...new Set(str.map(x => x))];
+
+console.log(uniqueChars.sort());
+
+let joined = uniqueChars.join('');
+
+let test = 'abcdefghijklmnopqrstuvwxyz';
+
+console.log(joined);
