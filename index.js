a0443a405a2ac649b0decfc1c096ce37734d23ac
Seth Bunke <seth.bunke@hotmail.com>
2016-09-29 00:54:30 +0000

    add comment for what to look at

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ let testArrayLeftRotation = () => {
     // let result1 = arrayLeftRotation.rotateArrayLeft([1,2,3,4,5], 4);   
     // assert([5,1,2,3,4], result1); 
 
+    //HANDLE IF THE NUMBER OF ROTATIONS IS LESS THAN THE LENGTH OF THE ARRAY
+
     let data2 = [431, 397, 149, 275, 556, 362, 852, 789, 601, 357, 516, 575, 670, 507, 127, 888, 284, 405, 806, 27, 495, 879, 976, 467, 342, 356, 908, 750, 769, 947, 425, 643, 754, 396, 653, 595, 108, 75, 347, 394, 935, 252, 683, 966, 553, 724, 629, 567, 93, 494, 693, 965, 328, 187, 728, 389, 70, 288, 509, 252, 449];
     let k2 = 48;     
 
